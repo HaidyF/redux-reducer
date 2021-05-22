f8defd3303f9303916fdb93fad15059e2bed7bb9
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-05-22 14:09:31 -0400

    commit tree e88ca361a640007cd69481028be9aec983e337cd

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,31 @@
-export function manageFriends(state, action){
-}
+// export function manageFriends(state = {friends: []}, action) {
+//     action = {
+//         type: "ADD_FRIEND",
+//         friend: {
+//           name: "Chrome Boi",
+//           homewtown: "NYC",
+//           id: 1
+//         }
+//     }
+
+//     switch(action.type){
+//         case 'ADD_FRIEND':
+//             return friends.push;
+//         default: return state;
+        
+//     }
+    
+//     action = {
+//         type: "REMOVE_FRIEND",
+//         friend: {
+//           name: "Chrome Boi",
+//           homewtown: "NYC",
+//           id: 1
+//         }
+//       }
+
+//     switch(action.type){
+//         case 'REMOVE_FRIEND':
+//             return friends.pop
+//     }
+// }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,14 @@
-export function managePresents(state, action){
+
+export function managePresents(state = {numberOfPresents: 0}, action){
+
+    // action = {type: "INCREASE"}
+    
+    switch(action.type) {
+        case 'INCREASE':
+            return { numberOfPresents: state.numberOfPresents + 1 };
+        default: 
+            return state;
+    }
 }
+
+
